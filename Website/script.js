28835a0838ae42928f1b4b9bc29e7750dbd37720
JoshCodeKing <99140741+JoshCodeKing@users.noreply.github.com>
2024-12-07 16:18:34 -0500

    Add files via upload

---
+++ Website/script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
